c459e5ff7f3b2eabc367d4f3fa76a2771c0bdef6
dengyu <313187515@qq.com>
2020-01-04 19:42:35 +0800

    获取路由

--- blogtest/app.js
+++ blogtest/app.js
@@ -13,20 +13,26 @@ const http = require('http')
 
 // })
 
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'POST') {
+//         console.log('req content-type:', req.headers['content-type']);
+//         let postData = '';
+//         req.on('data', chunk => {
+//             postData += chunk.toString();
+//         })
+//         req.on('end', () => {
+//             console.log('postData:', postData);
+
+//             res.end('hello world')
+//         })
+
+//     }
+// })
+
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        console.log('req content-type:', req.headers['content-type']);
-        let postData = '';
-        req.on('data', chunk => {
-            postData += chunk.toString();
-        })
-        req.on('end', () => {
-            console.log('postData:', postData);
-
-            res.end('hello world')
-        })
-
-    }
+    const url = req.url
+    const path = url.split('?')[0]
+    res.end(path)
 })
 server.listen(8000)
 console.log('ok');

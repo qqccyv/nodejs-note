31db64d7481a71a87e6de684eb58ddb3c9838327
dengyu <313187515@qq.com>
2020-01-04 10:09:58 +0800

    node基础功能实现

---
+++ blogbasic/helloworld.js
@@ -0,0 +1,18 @@
+var first = 'hello nodejs';
+console.log(first);
+
+function fun() {
+    console.log('fun函数被调用了');
+
+}
+
+fun();
+
+for (var i = 0; i < 2; i++) {
+    console.log('循环被执行了');
+
+}
+if (true) {
+    console.log('判断被执行了');
+
+}

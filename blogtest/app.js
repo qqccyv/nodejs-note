91270e8b81e329df903409a013bb815814090b11
dengyu <313187515@qq.com>
2020-01-04 20:40:41 +0800

    node处理请求的初步实现

--- blogtest/app.js
+++ blogtest/app.js
@@ -29,10 +29,45 @@ const http = require('http')
 //     }
 // })
 
+// const server = http.createServer((req, res) => {
+//     const url = req.url
+//     const path = url.split('?')[0]
+//     res.end(path)
+// })
+
+const querystring = require('querystring');
 const server = http.createServer((req, res) => {
-    const url = req.url
-    const path = url.split('?')[0]
-    res.end(path)
+    const method = req.method;
+    const url = req.url;
+    const query = querystring.parse(url.split('?')[1]);
+    const path = url.split('?')[0];
+    res.setHeader('content-type', 'application/json')
+    const resData = {
+        method,
+        url,
+        query,
+        path
+    }
+    if (method === 'GET') {
+        res.end(JSON.stringify(resData))
+    };
+
+
+    if (method === 'POST') {
+        // console.log('req content-type:', req.headers['content-type']);
+        let postData = '';
+        req.on('data', chunk => {
+            // console.log('chunk:', chunk);
+
+            postData += chunk.toString();
+        })
+        req.on('end', () => {
+            // console.log('postData:', postData);
+            resData.postData = postData;
+            res.end(JSON.stringify(resData))
+        })
+
+    }
 })
 server.listen(8000)
 console.log('ok');

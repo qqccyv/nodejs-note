672715007108b6fa7f588548e8c1f86df30fbf1c
dengyu <313187515@qq.com>
2020-01-05 15:14:27 +0800

    getList获取列表数据

--- blog1/app.js
+++ blog1/app.js
@@ -1,10 +1,15 @@
+const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 const serverHandle = (req, res) => {
 
     res.setHeader('content-type', 'application/json')
+
     const url = req.url
     req.path = url.split('?')[0];
+
+    req.query = querystring.parse(url.split('?')[1])
+
     const blogData = handleBlogRouter(req, res)
     if (blogData) {
         res.end(

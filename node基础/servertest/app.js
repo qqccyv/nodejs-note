c0eeb33f9eda8864d1ce77cf9759134dde03f63e
dengyu <313187515@qq.com>
2020-01-06 13:56:41 +0800

    用url方法同时获取路由与参数

--- node基础/servertest/app.js
+++ node基础/servertest/app.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const app = http.createServer();
+const url = require('url')
 app.on('request', (req, res) => {
     // res.end('<h1>hello world</h1>')
     res.writeHead(200, {
-        'content-type': 'text/html;charset-utf8'
-    })
-    if (req.url === '/index') {
+            'content-type': 'text/html;charset-utf8'
+        })
+        // console.log(req.url);
+        // console.log(url.parse(req.url, true));
+    let { pathname, query } = url.parse(req.url, true)
+    console.log(pathname);
+    console.log(query);
+
+    if (req.url === '/index' || req.url === '/') {
         res.end('index...')
     } else {
         res.end('404....')

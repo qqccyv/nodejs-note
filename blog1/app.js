c99306e4bf0cf3532de7062e67aa15c5f3671192
dengyu <313187515@qq.com>
2020-01-06 19:28:34 +0800

    异步处理postdata

--- blog1/app.js
+++ blog1/app.js
@@ -1,32 +1,65 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
-const serverHandle = (req, res) => {
 
-    res.setHeader('content-type', 'application/json')
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({});
+            return
+        }
+        let postData = '';
+        req.on('data', chunk => {
+            // console.log('chunk:', chunk);
 
-    const url = req.url
-    req.path = url.split('?')[0];
-
-    req.query = querystring.parse(url.split('?')[1])
-
-    const blogData = handleBlogRouter(req, res)
-    if (blogData) {
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return
-    }
-    const userData = handleUserRouter(req, res);
-    if (userData) {
-        res.end(
-            JSON.stringify(userData)
-        )
-        return
-    }
-
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.write("404 Not Found\n")
-    res.end()
+            postData += chunk.toString();
+        })
+        req.on('end', () => {
+            // console.log('postData:', postData);
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(JSON.parse(postData))
+        })
+    })
+    return promise
 }
+getPostData(req).then(postData => {
+    req.body = postData;
+    const serverHandle = (req, res) => {
+
+        res.setHeader('content-type', 'application/json')
+
+        const url = req.url
+        req.path = url.split('?')[0];
+
+        req.query = querystring.parse(url.split('?')[1])
+            // let { pathname, query } = url.parse(req.url, true)  url.parse方法简写
+
+        const blogData = handleBlogRouter(req, res)
+        if (blogData) {
+            res.end(
+                JSON.stringify(blogData)
+            )
+            return
+        }
+        const userData = handleUserRouter(req, res);
+        if (userData) {
+            res.end(
+                JSON.stringify(userData)
+            )
+            return
+        }
+
+        res.writeHead(404, { "content-type": "text/plain" });
+        res.write("404 Not Found\n")
+        res.end()
+    }
+})
+
 module.exports = serverHandle

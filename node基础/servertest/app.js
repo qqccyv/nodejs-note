f29ffa02ae8f18731677a84540ebf3d5cb27098f
dengyu <313187515@qq.com>
2020-01-06 10:50:26 +0800

    获取url

--- node基础/servertest/app.js
+++ node基础/servertest/app.js
@@ -2,6 +2,11 @@ const http = require('http');
 const app = http.createServer();
 app.on('request', (req, res) => {
     // res.end('<h1>hello world</h1>')
+    if (req.url === '/index') {
+        res.end('index...')
+    } else {
+        res.end('404....')
+    }
     if (req.method === 'GET') {
         res.end('get')
     } else if (req.method === 'POST') {

d03181b514b4f2f409df77fdab17d18e1ceac6ba
dengyu <313187515@qq.com>
2020-01-07 18:50:47 +0800

    添加登录路由

--- blog1/src/router/user.js
+++ blog1/src/router/user.js
@@ -1,10 +1,14 @@
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 const handleUserRouter = (req, res) => {
     const method = req.method;
-
+    const { username, password } = req.body
     if (method === 'POST' && req.path === '/api/user/login') {
-        return {
-            msg: '这是登录博客的接口'
+        const result = loginCheck(username, password)
+        if (result) {
+            return new SuccessModel('登录成功')
         }
+        return new ErrorModel('登录失败')
     }
 }
 module.exports = handleUserRouter;

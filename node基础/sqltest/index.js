e3d0412eb83da91fed26bc653507a1fd563e8901
dengyu <313187515@qq.com>
2020-01-08 15:04:52 +0800

    利用nodejs做mysql的增删改查

---
+++ node基础/sqltest/index.js
@@ -0,0 +1,25 @@
+mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '20152346',
+    port: '3306',
+    database: 'myblog'
+})
+con.connect();
+const sql = 'select*from blogs';
+// const sql = 'select*from users where username="lisi"';
+// const sql = 'update users set realname="李四2" where username="lisi"';
+// const sql = 'select*from users order by id desc';
+// const sql = 'insert into blogs(title,content,createtime,author) values("标题C","内容C",1578466757600,"zhansan")'
+
+con.query(sql, (err, result) => {
+    if (err) {
+        console.error(err);
+        return
+
+    }
+    console.log(result);
+
+})
+con.end()

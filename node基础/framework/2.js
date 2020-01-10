34b9581a9d7acf1f68e083697bb91741bbeebdcd
dengyu <313187515@qq.com>
2020-01-10 16:14:33 +0800

    同步错误和异步错误的区别

--- node基础/framework/2.js
+++ node基础/framework/2.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 app.get('/index', (req, res, next) => {
-    fs.readFile('./01.js1', 'utf8', (err, doc) => {
-        if (err != null) {
-            next(err)
-        } else {
-            res.send(doc)
-        }
-    })
+    // fs.readFile('./01.js1', 'utf8', (err, doc) => {
+    //     if (err != null) {
+    //         next(err)
+    //     } else {
+    //         res.send(doc)
+    //     }
+    // })
+    throw new Error('1111111')
 })
 app.use((err, req, res) => {
-    res.status(500).send(err.message)
+    res.status(500).send(err)
 })
 
 

d0bdd51ade3acfc15005bd37a56a66c40f5a57ad
dengyu <313187515@qq.com>
2020-01-14 15:06:02 +0800

    添加点击爱心特效

--- blogfinal/public/admin/js/common.js
+++ blogfinal/public/admin/js/common.js
@@ -6,3 +6,35 @@ function serializeToJson(form) {
     });
     return result;
 }
+
+function animate(obj, target, callback) {
+
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+
+        if (obj.offsetTop == target) {
+            clearInterval(obj.timer);
+            if (callback) {
+                callback();
+            }
+        }
+        obj.style.top = obj.offsetTop - 1 + 'px';
+
+    }, 15);
+}
+document.onclick = function(e) {
+    var box = document.createElement('div');
+    var heart = document.createTextNode("❤");
+    box.appendChild(heart)
+    box.style.position = 'absolute';
+    box.style.left = e.clientX + 'px';
+    box.style.top = e.clientY + 'px';
+    box.style.color = "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
+    box.style.zIndex = 100000;
+
+    document.body.appendChild(box);
+    let target = e.clientY - 20
+    animate(box, target, function() {
+        document.body.removeChild(box);
+    })
+}

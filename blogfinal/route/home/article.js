534b6c482e6a35d9203c3f2725e1e79f742d27aa
dengyu <313187515@qq.com>
2020-01-20 11:15:16 +0800

    添加展示评论路由，bug修复，模板无法找到评论数据，我也不晓得为撒子

--- blogfinal/route/home/article.js
+++ blogfinal/route/home/article.js
@@ -1,13 +1,16 @@
 const { Article } = require('../../model/article')
+const { Comment } = require('../../model/comment')
 module.exports = async(req, res, next) => {
 
-    const id = req.query.id
+    const { id } = req.query
     let article = await Article.findOne({ _id: id }).populate('author')
-    console.log(req.session.username);
+    let comment = await Comment.find({ aid: id }).populate('uid')
+        // console.log(comment.uid.username);
 
     res.render('home/article', {
             article,
-            username: req.session.username
+            username: req.session.username,
+            comment
         })
         // res.rend('欢迎来到博客详情页')
 
--- blogfinal/route/home/comment.js
+++ blogfinal/route/home/comment.js
@@ -1,3 +1,7 @@
-module.exports = (req, res) => {
-    res.send('ok')
+const { Comment } = require('../../model/comment')
+
+module.exports = async(req, res) => {
+    const { content, uid, aid } = req.body
+    await Comment.create({ content, uid, aid, time: new Date() })
+    res.redirect(`/home/article?id=${aid}`)
 }
